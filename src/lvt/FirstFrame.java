129785529fc5e78dba4db2495df2645d164711b2
Starceva <anzekikastar0806@gmail.com>
2024-05-27 17:56:21 +0300

    started project

---
+++ src/lvt/FirstFrame.java
@@ -0,0 +1,48 @@
+package lvt;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+public class FirstFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FirstFrame frame = new FirstFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FirstFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 480);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		
+		JLabel BackGround = new JLabel("");
+		BackGround.setIcon(new ImageIcon("D:\\Bum\\Starceva_Eksamens\\Background1.png"));
+		contentPane.add(BackGround);
+	}
+
+}
